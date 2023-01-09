2134b684c3192177c265b795fdd26e3536af2cb1
Томко Владислав Викторович <tomko@dev.vtb.ru>
2023-01-09 12:04:11 +0300

    add Dockerfiler

--- conveor/src/main/java/ru/neoflex/conveor/controller/ConveyorController.java
+++ conveor/src/main/java/ru/neoflex/conveor/controller/ConveyorController.java
@@ -1,11 +1,9 @@
 package ru.neoflex.conveor.controller;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import ru.neoflex.conveor.model.CreditDTO;
 import ru.neoflex.conveor.model.LoanApplicationRequestDTO;
 import ru.neoflex.conveor.model.LoanOfferDTO;
@@ -13,6 +11,7 @@ import ru.neoflex.conveor.model.ScoringDataDTO;
 import ru.neoflex.conveor.service.CalculatorService;
 import ru.neoflex.conveor.service.ConveyorService;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -31,12 +30,21 @@ public class ConveyorController {
     private final ConveyorService conveyorService;
 
     @PostMapping(OFFERS_PATH)
-    public ResponseEntity<List<LoanOfferDTO>> offers(@RequestBody LoanApplicationRequestDTO req) {
+    public ResponseEntity<List<LoanOfferDTO>> offers(@RequestBody @Valid LoanApplicationRequestDTO req, BindingResult bindingResult) {
+        System.out.println("bindingResult.hasErrors() + " + bindingResult.hasErrors());
+        if (bindingResult.hasErrors()) {
+            throw new RuntimeException();
+        }
         return ResponseEntity.ok(conveyorService.prepareOffers(req));
     }
 
     @PostMapping(CALCULATION_PATH)
     public ResponseEntity<CreditDTO> calculation(@RequestBody ScoringDataDTO req) {
         return ResponseEntity.ok(calculatorService.calculateCredit(req));
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<String> dockerTest() {
+        return ResponseEntity.ok("Hello Docker!");
     }
 }
